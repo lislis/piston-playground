544ff5edc55984fce00a62301be7963ee5a6b5d1
lislis <mail@lislis.de>
2017-04-02 23:53:37 +0200

    add font

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use piston_window::*;
 fn main() {
     let opengl = OpenGL::V3_2;
     let mut window: PistonWindow =
-        WindowSettings::new("draw an image", [300, 600])
+        WindowSettings::new("draw an image", [400, 600])
         .exit_on_esc(true)
         .opengl(opengl)
         .build()
@@ -21,14 +21,27 @@ fn main() {
         Flip::None,
         &TextureSettings::new()
     ).unwrap();
-    window.set_lazy(true);
 
+    let ref font = assets.join("Jomhuria-Regular.ttf");
+    let factory = window.factory.clone();
+    let mut glyphs = Glyphs::new(font, factory).unwrap();
+
+    window.set_lazy(true);
     // game loop
     // iterate over window events
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g| {
+            let font_transform = c.transform.trans(10.0, 100.0);
+
             clear([1.0; 4], g);
-            image(&house, c.transform, g);
+            image(&house, c.transform.trans(50.0, 0.0), g);
+            text::Text::new_color([1.0, 0.0, 0.0, 1.0], 32).draw(
+                "POINTS",
+                &mut glyphs,
+                &c.draw_state,
+                font_transform,
+                g
+            );
         });
     }
 }
