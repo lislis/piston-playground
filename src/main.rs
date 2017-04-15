375e43f40db77468cf8eac245cbce3d83729e4bd
lislis <mail@lislis.de>
2017-04-15 19:22:42 +0200

    create one throwable

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,36 @@ use player::Player;
 
 use piston_window::*;
 
+struct Throwable {
+    pub active: bool,
+    pub x: f64,
+    pub y: f64,
+}
+
+impl Throwable {
+    pub fn new() -> Throwable {
+        Throwable {
+            active: false,
+            x: 0.0,
+            y: 0.0
+        }
+    }
+    pub fn set_active(&mut self, x: f64, y: f64) {
+        self.active = true;
+        self.x = x;
+        self.y = y;
+    }
+    pub fn update(&mut self, delta_time: f64) {
+        if self.active {
+            self.x += delta_time;
+        }
+    }
+}
+
+fn calc_dis_pos(pos: f64, factor: f64) -> f64 {
+    factor + (pos * factor)
+}
+
 fn main() {
     let opengl = OpenGL::V3_2;
     let mut window: PistonWindow =
@@ -20,6 +50,8 @@ fn main() {
 
     let mut apples = Apples::new();
     let mut player = Player::new();
+
+    let COLOR = [0.3, 0.5, 0.5, 1.0];
 
     let assets = find_folder::Search::ParentsThenKids(3,3)
         .for_folder("assets").unwrap();
@@ -49,9 +81,9 @@ fn main() {
     let factory = window.factory.clone();
     let mut glyphs = Glyphs::new(font, factory).unwrap();
 
-    let color = [0.3, 0.5, 0.5, 1.0];
-
     let score = "Points: ";
+
+    let mut throwable = Throwable::new();
 
     let drawfactor = 90.0;
 
@@ -61,7 +93,8 @@ fn main() {
     while let Some(e) = window.next() {
 
         if let Some(upd) = e.render_args() {
-
+            // println!("{:?}", upd);
+            // throwable.update(upd.ext_dt);
         }
 
         if let Some(button) = e.release_args() {
@@ -79,6 +112,11 @@ fn main() {
                 Button::Keyboard(Key::D) => {
                     player.moving(1, 0);
                 }
+                Button::Keyboard(Key::K) => {
+                    let x = calc_dis_pos(player.x as f64, drawfactor);
+                    let y = calc_dis_pos(player.y as f64, drawfactor);
+                    throwable.set_active(x, y);
+                }
                 _ => {}
             }
         }
@@ -88,7 +126,7 @@ fn main() {
 
             clear([1.0; 4], g);
             image(&house, c.transform.trans(50.0, 0.0), g);
-            text::Text::new_color(color, 42).draw(
+            text::Text::new_color(COLOR, 42).draw(
                 score,
                 &mut glyphs,
                 &c.draw_state,
@@ -108,11 +146,20 @@ fn main() {
                 for (ri, rv) in (0..player.columns).enumerate() {
                     if rv == player.x && cv == player.y {
                         let square = rectangle::square(0.0, 0.0, 50.0);
-                        rectangle(color, square, c.transform.trans(
-                            (drawfactor + (drawfactor * rv as  f64)),
-                            (drawfactor + (drawfactor * cv as f64))), g);
+                        rectangle(COLOR, square, c.transform.trans(
+                            calc_dis_pos(rv as f64, drawfactor),
+                            calc_dis_pos(cv as f64, drawfactor)), g);
+                        image(&apple, c.transform.trans(
+                            calc_dis_pos(rv as f64, drawfactor),
+                            calc_dis_pos(cv as f64, drawfactor)).scale(0.9, 0.9), g);
                     }
+
                 }
+            }
+
+            if throwable.active {
+                image(&apple, c.transform.trans(
+                    throwable.x, throwable.y), g);
             }
         });
 
